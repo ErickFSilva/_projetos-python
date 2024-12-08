bc368060fd5031321a48264eb181517c2d3f601c
ErickFSilva <erick_ferreira@outlook.com>
2024-12-08 11:17:17 -0300

    Curso Python 3, 08.12.2024 (manhã)

--- curso_python_3/secao_03_iniciando_programacao_python/aula018_funcao_print.py
+++ curso_python_3/secao_03_iniciando_programacao_python/aula018_funcao_print.py
@@ -1,18 +1,24 @@
 # Função print()
 
 """
-Utilizada para exibir conteúdos em tela
-Ela recebe argumentos
-Os argumentos são separados por vírgula
-Por padrão, é adicionado um espaço entre a exibição de um argumento e outro. (São os argumentos não nomeados)
-Para remover esses espaços impressos, utilizamos o argumento: sep=''
+Utilizada para exibir conteúdos em tela;
+Ela recebe argumentos;
+Os argumentos são separados por vírgula;
+Por padrão, é adicionado um espaço entre a exibição de um argumento e outro. (São os argumentos não nomeados);
+O argumento sep='' ou sep="" é utilizado para remover esses espaços impressos;
+O argumento end='' ou end="" é utilizado no último argumento do 'print()';
+Para quebrar a linha utiliza-se '\n' dentro de aspas simples ou duplas;
 """
 
-# Argumentos não nomeados
+# Argumentos não nomeados:
+print('- Argumentos não nomados:')
 print(12, 34)
+print(56,78,'\n')
 
-# Argumentos nomeados
-print(56, 78, sep='')
-print(90, 12, sep=' ')
-print(34, 56, sep='  ')
-print(78, 90, sep='-')
+# Argumentos nomeados: 'sep' = separador
+print("- Argumentos nomeados:")
+print(12, 34, sep="-")
+print(56, 78, sep=', ')
+print(90, 12, sep="")
+print('abc', end=" <-\n-> ")
+print('def')
